c8a8ece9fc926b01c724d463648e30ef61fc97bf
Luis Santos <luis.santos@nfsconsultoria.com.br>
2021-08-20 17:16:53 -0300

    Conclusão do fork

--- app/src/main/java/br/com/heiderlopes/pokemonwstemplatev2/data/repository/PokemonRespositoryImpl.kt
+++ app/src/main/java/br/com/heiderlopes/pokemonwstemplatev2/data/repository/PokemonRespositoryImpl.kt
@@ -2,6 +2,8 @@ package br.com.heiderlopes.pokemonwstemplatev2.data.repository
 
 import br.com.heiderlopes.pokemonwstemplatev2.data.remote.api.PokemonService
 import br.com.heiderlopes.pokemonwstemplatev2.data.remote.mapper.PokemonResponseListToPokemonListMapper
+import br.com.heiderlopes.pokemonwstemplatev2.data.remote.mapper.PokemonResponseToPokemonMapper
+import br.com.heiderlopes.pokemonwstemplatev2.data.remote.mapper.PokemonToUpdatePokemonRequestMapper
 import br.com.heiderlopes.pokemonwstemplatev2.domain.model.Pokemon
 import br.com.heiderlopes.pokemonwstemplatev2.domain.repository.PokemonRepository
 
@@ -9,10 +11,22 @@ class PokemonRespositoryImpl(
     val pokemonService: PokemonService
 ) : PokemonRepository {
 
-    private val pokemonResponseListToPokemonListMapper = PokemonResponseListToPokemonListMapper()
+    private val pokemonListMapper = PokemonResponseListToPokemonListMapper()
+    private val pokemonMapper: PokemonResponseToPokemonMapper = PokemonResponseToPokemonMapper()
+    private val updatePokemonRequestMapper: PokemonToUpdatePokemonRequestMapper = PokemonToUpdatePokemonRequestMapper()
 
     override suspend fun getPokemons(size: Int, sort: String): Result<List<Pokemon>> {
         return Result.success(
-            pokemonResponseListToPokemonListMapper.map(pokemonService.getPokemons(size, sort).pokemons))
+            pokemonListMapper.map(pokemonService.getPokemons(size, sort).pokemons))
+    }
+
+    override suspend fun getPokemon(number: String): Result<Pokemon> {
+        return Result.success(pokemonMapper.map(pokemonService.getPokemon(number)))
+    }
+
+    override suspend fun update(pokemon: Pokemon): Result<Pokemon> {
+        return Result.success(pokemonMapper.map(
+            pokemonService.updatePokemon(updatePokemonRequestMapper.map(pokemon))
+        ))
     }
 }
--- app/src/main/java/br/com/heiderlopes/pokemonwstemplatev2/presentation/formpokemon/FormPokemonActivity.kt
+++ app/src/main/java/br/com/heiderlopes/pokemonwstemplatev2/presentation/formpokemon/FormPokemonActivity.kt
@@ -2,11 +2,100 @@ package br.com.heiderlopes.pokemonwstemplatev2.presentation.formpokemon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AbsSeekBar
+import android.widget.SeekBar
+import android.widget.TextView
+import android.widget.Toast
 import br.com.heiderlopes.pokemonwstemplatev2.R
+import br.com.heiderlopes.pokemonwstemplatev2.databinding.ActivityFormPokemonBinding
+import br.com.heiderlopes.pokemonwstemplatev2.domain.model.Pokemon
+import br.com.heiderlopes.pokemonwstemplatev2.presentation.model.ViewState
+import com.squareup.picasso.Picasso
+import org.koin.android.ext.android.inject
+import org.koin.android.viewmodel.ext.android.viewModel
 
-class FormPokemonActivity : AppCompatActivity() {
+class FormPokemonActivity : AppCompatActivity(R.layout.activity_form_pokemon) {
+
+    private val formPokemonViewModel: FormPokemonViewModel by viewModel()
+    val picasso: Picasso by inject()
+    private lateinit var pokemon: Pokemon
+
+    private val viewBinding by lazy {
+        ActivityFormPokemonBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_form_pokemon)
+        setContentView(viewBinding.root)
+
+        val pokemonNumber = intent.getStringExtra("POKEMON") ?: ""
+        formPokemonViewModel.getPokemon(pokemonNumber)
+
+        viewBinding.btSaveForm.setOnClickListener {
+            pokemon.attack = viewBinding.sbAttack.progress
+            pokemon.defense = viewBinding.sbDefense.progress
+            pokemon.velocity = viewBinding.sbPS.progress
+            formPokemonViewModel.update(
+                pokemon
+            )
+        }
+        registerObserve()
+    }
+
+    private fun registerObserve() {
+        formPokemonViewModel.pokemonResult.observe(this, {
+            when(it) {
+                is ViewState.Success -> setValues(it.data)
+                is ViewState.Loading -> {}
+                is ViewState.Failure -> Toast.makeText(
+                    this, it.throwable.message, Toast.LENGTH_LONG
+                ).show()
+            }
+        })
+        formPokemonViewModel.pokemonUpdateResult.observe(this, {
+            when(it) {
+                is ViewState.Success -> Toast.makeText(
+                        this, getString(R.string.pokemon_update_sucess), Toast.LENGTH_LONG
+                    ).show()
+                is ViewState.Loading -> {}
+                is ViewState.Failure -> Toast.makeText(
+                    this, it.throwable.message, Toast.LENGTH_LONG
+                ).show()
+            }
+        })
+    }
+
+    private fun setValues(pokemon: Pokemon) {
+        this.pokemon = pokemon
+        viewBinding.tvPokemonNameForm.text = pokemon.name
+
+        picasso.load("https://pokedexdx.herokuapp.com${pokemon.imageURL}")
+            .into(viewBinding.ivPokemonForm)
+
+        viewBinding.sbAttack.progress = pokemon.attack
+        viewBinding.sbDefense.progress = pokemon.defense
+        viewBinding.sbPS.progress = pokemon.ps
+        viewBinding.sbVelocity.progress = pokemon.velocity
+
+        viewBinding.tvAttack.text = pokemon.attack.toString()
+        viewBinding.tvDefense.text = pokemon.defense.toString()
+        viewBinding.tvPS.text = pokemon.ps.toString()
+        viewBinding.tvVelocity.text = pokemon.velocity.toString()
+
+        setListener(viewBinding.sbAttack, viewBinding.tvAttackValue)
+        setListener(viewBinding.sbDefense, viewBinding.tvDefenseValue)
+        setListener(viewBinding.sbPS, viewBinding.tvPSValue)
+        setListener(viewBinding.sbVelocity, viewBinding.tvVelocityValue)
+    }
+
+    private fun setListener(seekBar: SeekBar, textView: TextView) {
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                textView.text = progress.toString()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
     }
 }
